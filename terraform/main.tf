175dfbd825b045f4c4d700e1a69a1e3874e889c0
ramganesh777 <52460386+ramganesh777@users.noreply.github.com>
2022-01-19 14:17:06 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,36 @@ module "sandbox_account_01" {
   account_customizations_name = "sandbox"
 }
 
+module "sandbox_account_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "ramgcttest10@gmail.com"
+    AccountName  = "sandboxfix"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "ramgcttest10@gmail.com"
+    SSOUserFirstName = "ram"
+    SSOUserLastName  = "yash"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "ramgcttest10@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Ramg"
+    change_reason       = "testing the account vending process"
+  }
+  
+  account_customizations_name = "sandbox"
+}

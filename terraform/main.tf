7cefbc65f12cbd89c9c48f5b18baeb221f48cdd9
ramganesh777 <52460386+ramganesh777@users.noreply.github.com>
2022-01-19 14:03:49 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
-module "sandbox" {
+module "sandbox_account_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "ramgcttest10@gmail.com"
+    AccountEmail              = "ramgcttest9@gmail.com"
     AccountName               = "sandboxnew"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "ramgcttest10@gmail.com"
+    SSOUserEmail              = "ramgcttest9@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "New"
   }
 
   
   account_tags = {
-    "ABC:Owner"       = "ramgcttest10@gmail.com"
+    "ABC:Owner"       = "ramgcttest9@gmail.com"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
@@ -31,5 +31,39 @@ module "sandbox" {
     group = "non-prod"
   }
 
+  account_customizations_name = "sandbox"
+}
+
+module "sandbox_account_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "ramgcttest10@gmail.com"
+    AccountName  = "sandboxfix"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "ramgcttest10@gmail.com
+    SSOUserFirstName = "ram"
+    SSOUserLastName  = "yash"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "ramgcttest10@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Ramg"
+    change_reason       = "testing the account vending process"
+  }
+  
   account_customizations_name = "sandbox"
 }
